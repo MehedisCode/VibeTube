8a93caeb111ddd9170171bd19217f39cbcb05701
Md Mehedi Hasan <mehedialways@gmail.com>
2024-01-14 18:52:25 +0600

    some util files added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const {connectDB} = require('./db/index')
-const dotenv = require('dotenv')
+const express = require("express");
+const { connectDB } = require("./db/index");
+const dotenv = require("dotenv");
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-const app = express()
+const app = express();
 
 connectDB()
-
+  .then(() => {
+    app.listen(
+      process.env.PORT || 8000,
+      `Listening on port ${process.env.PORT}`
+    );
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+  });
