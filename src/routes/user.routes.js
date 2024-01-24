66f378b14fb59851d00457e98b3ab0aae8f6f124
Md Mehedi Hasan <mehedialways@gmail.com>
2024-01-24 17:32:56 +0600

    Add new routes for changing password, updating account details, and retrieving user profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ const {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountFullname,
+  updateAccountAvatar,
+  updateAccountCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } = require("../controllers/user.controller");
 const { upload } = require("../middlewares/multer.middleware.js");
 const { verifyJWT } = require("../middlewares/auth.middleware.js");
@@ -28,5 +35,16 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account").post(verifyJWT, updateAccountFullname);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.none("avatar"), updateAccountAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.one("coverImage"), updateAccountCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 module.exports = router;
